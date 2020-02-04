2275758363ec413965498e12a792af83ff0e6090
Franeman <franeman@yahoo.com>
2020-02-03 22:49:26 -0500

    Added maps and ammo tabs. Got maps working

--- EFTTool/Form1.Designer.cs
+++ EFTTool/Form1.Designer.cs
@@ -28,13 +28,103 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.mapPage = new System.Windows.Forms.TabPage();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.ammoPage = new System.Windows.Forms.TabPage();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.mapPage.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.mapPage);
+            this.tabControl1.Controls.Add(this.ammoPage);
+            this.tabControl1.Location = new System.Drawing.Point(13, 25);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1879, 1004);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // mapPage
+            // 
+            this.mapPage.Controls.Add(this.pictureBox1);
+            this.mapPage.Location = new System.Drawing.Point(4, 22);
+            this.mapPage.Name = "mapPage";
+            this.mapPage.Padding = new System.Windows.Forms.Padding(3);
+            this.mapPage.Size = new System.Drawing.Size(1871, 978);
+            this.mapPage.TabIndex = 0;
+            this.mapPage.Text = "Maps";
+            this.mapPage.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(6, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1858, 968);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // ammoPage
+            // 
+            this.ammoPage.Location = new System.Drawing.Point(4, 22);
+            this.ammoPage.Name = "ammoPage";
+            this.ammoPage.Padding = new System.Windows.Forms.Padding(3);
+            this.ammoPage.Size = new System.Drawing.Size(1871, 978);
+            this.ammoPage.TabIndex = 1;
+            this.ammoPage.Text = "Ammo";
+            this.ammoPage.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(174, 6);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(106, 9);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(31, 13);
+            this.lbl1.TabIndex = 3;
+            this.lbl1.Text = "Map:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "EFTTool";
+            this.tabControl1.ResumeLayout(false);
+            this.mapPage.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage mapPage;
+        private System.Windows.Forms.TabPage ammoPage;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- EFTTool/Form1.cs
+++ EFTTool/Form1.cs
@@ -4,17 +4,78 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using HtmlAgilityPack;
 
 namespace EFTTool
 {
     public partial class Form1 : Form
     {
+        int currentMapIndex = 0;
+        int currentAmmoIndex = 0;
+        private string[] maps = new string[] {"Customs","Factory", "Interchange", "Labs - Basement", "Labs - 1st Floor", "Labs - 2nd Floor", "Reserve", "Shoreline", "Woods"};
+        private Image[] mapImages = new Image[] { Resource1.CustomsMap, Resource1.FactoryMap,Resource1.InterchangeMap, Resource1.LabsBasementMap, Resource1.LabsFirstFloorMap, Resource1.LabsSecondFloorMap,Resource1.ReserveMap,
+            Resource1.ShorelineMap, Resource1.WoodsMap };
+        private string[] ammo = new string[] {".366 TKM","12.7x108mm","12.7x55mm STs-130","12x70mm","20x70mm","4.6x30mm HK","5.45x39mm","5.56x45mm NATO","5.7x28mm FN","7.62x25mm Tokarev","7.62x39mm","7.62x51mm NATO","7.62x54mmR",
+            "9x18mm Makarov","9x19mm Parabellum","9x21mm Gyurza","9x39mm"};
+
         public Form1()
         {
             InitializeComponent();
+            InitializeMapTab();
+            ParsePage("https://escapefromtarkov.gamepedia.com/7.62x39mm");
         }
+
+        private async void ParsePage(string url)
+        {
+            HttpClient httpClient = new HttpClient();
+            var html = await httpClient.GetStringAsync(url); // Get page html
+
+            HtmlAgilityPack.HtmlDocument htmlDocument = new HtmlAgilityPack.HtmlDocument();
+            htmlDocument.LoadHtml(html);
+
+            var tableHtml = htmlDocument.DocumentNode.Descendants("table");
+
+            var tableList = tableHtml.Take(1);
+        }
+
+        private void InitializeMapTab()
+        {
+            lbl1.Text = "Map:"; // Set correct label for drop down menu
+            comboBox1.Items.Clear(); // Clear current list of items in drop menu
+
+            foreach (string map in maps) // Add all maps to drop menu
+            {
+                comboBox1.Items.Add(map);
+            }
+
+            comboBox1.SelectedIndex = currentMapIndex; // Set the current map
+            pictureBox1.Image = mapImages[currentMapIndex];
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            currentMapIndex = comboBox1.SelectedIndex;
+            pictureBox1.Image = mapImages[currentMapIndex];
+        }
+    }
+
+    struct Ammo
+    {
+        Image icon;
+        string name;
+        uint damage;
+        uint pen; // penetration power
+        uint armorDamage; // Armor Damage %
+        int accuracy; // Accuracy %
+        int recoil; // Recoil %
+        float fragChance; // Fragmentation chance
+        float ricochetChance;
+        uint speed; // Projectile Speed (m/s)
+        string specialEffects;
+        string seller; // Sold by
     }
 }
