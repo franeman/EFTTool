c548443ffc285f00d084a60dbb50ba6a4ce9a58a
Franeman <franeman@yahoo.com>
2020-02-09 14:22:45 -0500

    Added persistant map/ammo selection across tabs

--- EFTTool/Form1.Designer.cs
+++ EFTTool/Form1.Designer.cs
@@ -48,6 +48,7 @@
             this.tabControl1.SelectedIndex = 0;
             this.tabControl1.Size = new System.Drawing.Size(1879, 1004);
             this.tabControl1.TabIndex = 0;
+            this.tabControl1.SelectedIndexChanged += new System.EventHandler(this.tabControl1_SelectedIndexChanged);
             // 
             // mapPage
             // 
--- EFTTool/Form1.cs
+++ EFTTool/Form1.cs
@@ -14,13 +14,18 @@ namespace EFTTool
 {
     public partial class Form1 : Form
     {
-        int currentMapIndex = 0;
-        int currentAmmoIndex = 0;
-        private string[] maps = new string[] {"Customs","Factory", "Interchange", "Labs - Basement", "Labs - 1st Floor", "Labs - 2nd Floor", "Reserve", "Shoreline", "Woods"};
+        int currentMapIndex = 0; // Selected map
+        int currentAmmoIndex = 0; // Selected ammo type
+
+        private string[] maps = new string[] {"Customs","Factory", "Interchange", "Labs - Basement", "Labs - 1st Floor", "Labs - 2nd Floor", "Reserve", "Shoreline", "Woods"}; // Map names
+
         private Image[] mapImages = new Image[] { Resource1.CustomsMap, Resource1.FactoryMap,Resource1.InterchangeMap, Resource1.LabsBasementMap, Resource1.LabsFirstFloorMap, Resource1.LabsSecondFloorMap,Resource1.ReserveMap,
-            Resource1.ShorelineMap, Resource1.WoodsMap };
-        private string[] ammo = new string[] {".366 TKM","12.7x108mm","12.7x55mm STs-130","12x70mm","20x70mm","4.6x30mm HK","5.45x39mm","5.56x45mm NATO","5.7x28mm FN","7.62x25mm Tokarev","7.62x39mm","7.62x51mm NATO","7.62x54mmR",
-            "9x18mm Makarov","9x19mm Parabellum","9x21mm Gyurza","9x39mm"};
+            Resource1.ShorelineMap, Resource1.WoodsMap }; // Map images
+
+        private string[] ammo = new string[] {".366 TKM","4.6x30mm HK","5.7x28mm FN","5.45x39mm","5.56x45mm NATO","7.62x25mm Tokarev","7.62x39mm","7.62x51mm NATO","7.62x54mmR","9x18mm Makarov","9x19mm Parabellum","9x21mm Gyurza",
+            "9x39mm","12.7x55mm STs-130","12.7x108mm","12x70mm","20x70mm"}; // Ammo type names
+
+        private string[] urls = new string[] { "" }; // wiki links to ammo type pages
 
         public Form1()
         {
@@ -56,10 +61,44 @@ namespace EFTTool
             pictureBox1.Image = mapImages[currentMapIndex];
         }
 
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        private void InitializeAmmoTab()
         {
-            currentMapIndex = comboBox1.SelectedIndex;
-            pictureBox1.Image = mapImages[currentMapIndex];
+            lbl1.Text = "Ammo:"; // Set correct label for drop down menu
+            comboBox1.Items.Clear(); // Clear current list of items in drop menu
+
+            foreach (string ammo in ammo) // Add all ammo types to drop menu
+            {
+                comboBox1.Items.Add(ammo);
+            }
+
+            comboBox1.SelectedIndex = currentAmmoIndex; // Set the current ammo type
+
+        }
+
+            private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (tabControl1.SelectedIndex == 0) // On map tab
+            {
+                currentMapIndex = comboBox1.SelectedIndex;
+                pictureBox1.Image = mapImages[currentMapIndex];
+            }
+            else // On ammo tab
+            {
+                currentAmmoIndex = comboBox1.SelectedIndex;
+            }
+        }
+
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch(tabControl1.SelectedIndex)
+            {
+                case 0: // Map tab selected
+                    InitializeMapTab();
+                    break;
+                case 1: // Ammo tab selected
+                    InitializeAmmoTab();
+                    break;
+            }
         }
     }
 
